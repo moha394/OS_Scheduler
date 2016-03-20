7420d0a8f256ce6254a103220cc4fc5aba798649
Mohamed Ahmed <moha2014394@gmail.com>
2016-03-20 22:37:04 +0200

    FCFS modified

--- fcfs.cpp
+++ fcfs.cpp
@@ -4,67 +4,20 @@
 
 void Scheduler::fcfs ()
 {
-	int time = 0;
+	int Time = 0;
 	double total_waiting_time =0 ,avg_waiting_time , temp=0 ; 
-	pointer = _queue.begin();
-	       
-	    while(pointer != _queue.end()) ////// put the next arrival time in temp 
-	    {
-		    ++pointer ;
-			temp = pointer->getArrivalTime() ;
-		}
-
-		while(pointer != _queue.end()) ///calculate the total Waiting time
-	       {
-		       pointer->setWaitingTime(0);pointer->setRemainingTime(0);
-			   if((pointer->get_BurstTime() + pointer-> getArrivalTime()) < temp )
-			   {
-				   time = (pointer->get_BurstTime() + pointer->getArrivalTime()) - temp ;
-				   pointer-> setWaitingTime(time);
-				   
-			   }
-			   else {pointer->setWaitingTime(0);}
-			   total_waiting_time += pointer->get_WaitingTime();
-		   }
-
-		if(pointer == _queue.end())///calculate the Avg Time 
-		{
-				avg_waiting_time= total_waiting_time/_queue.size();			
-			    cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;
-
-		}		
-
-		
-
-
-
-  /*     while(pointer != _queue.end())
-    {
-       pointer->setWaitingTime(time);
-	   total_waiting_time += time;
-
-	   pointer->set_remainingTime(0);
-	   time=time + pointer->get_BurstTime() ;
-	   ++pointer;
+	list<Process>::iterator pointer;
+	
+	while(pointer != _queue.end())
+	{
+		pointer->start(Time);
+		Time = Time + pointer->getBurstTime();	
+		pointer -> stop(Time);
+		total_waiting_time+=pointer->getWaitingTime();
+		++pointer;
 	}
-
-	pointer=_queue.begin();
-
-    total_waiting_time=0;
-
-    for(int i=1;i<max;i++)
-
-    {
-
-        pointer ++;
-
-        total_waiting_time=total_waiting_time+(pointer->wait_time);
-
-    }
-
-    avg_waiting_time=total_waiting_time/max;
-
-    cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;*/
-
+	avg_waiting_time= total_waiting_time/_queue.size();
+		 cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;
 
 }
+	
