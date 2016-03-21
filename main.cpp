631ebebb7e767fa7e8c73a98be5ced42a0e4a163
Mo'men Adel <mouemened@gmail.com>
2016-03-21 15:28:28 +0200

    main testing Priority

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main()
 {
 	list<Process*> q;
-	q.push_back(new Process(0, 0, 8));
-	q.push_back(new Process(1, 1, 4));
-	q.push_back(new Process(2, 2, 9));
-	q.push_back(new Process(3, 3, 5));
+	q.push_back(new Process(1, 2, 5, 5));
+	q.push_back(new Process(2, 4, 5, 1));
+	// q.push_back(new Process(3, 0, 2, 4));
+	// q.push_back(new Process(4, 0, 1, 5));
+	// q.push_back(new Process(5, 0, 5, 2));
 	Scheduler s = Scheduler(q);
-	s.sjf(true);
+	s.priority();
 	return 0;
 }
