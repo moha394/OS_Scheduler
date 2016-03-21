3c0fe344c7101e840736c5768d3f49e11f3e287a
Mohamed Ahmed <moha2014394@gmail.com>
2016-03-21 19:35:08 +0200

    Final Edit Round Robin

--- roundRobin.cpp
+++ roundRobin.cpp
@@ -12,9 +12,12 @@ void Scheduler::roundRobin ()
 		cin >> x ; 
 		double z=_queue.size();
 		double Time =0 ;
+		
+		
+
 		while (!_queue.empty())
 		{
-		if(pointer == _queue.end)
+		if(pointer == _queue.end())
 		{
 		pointer=_queue.begin();	
 		}
@@ -22,13 +25,23 @@ void Scheduler::roundRobin ()
 		{
 			if((*pointer)->getArrivalTime() <=Time)
 			{
-			(*pointer)->start(Time);
-			Time += x;
-			(*pointer) ->stop(Time);
-			if((*pointer)->finished())
+				(*pointer)->start(Time);
+				if((*pointer)->getRemainingTime(Time) < x)
+					Time += (*pointer)->getRemainingTime(Time);
+				else
+					Time += x;
+				(*pointer) ->stop(Time);
+			if((*pointer)->isFinished())
 				{
 					total_waiting_time+= (*pointer)->getWaitingTime();
-					_queue.erase(pointer);
+					if(_queue.size() == 1) {
+						_queue.erase(pointer);
+						break;
+					}
+					else {
+						--pointer;
+						_queue.erase(next(pointer,1));
+					}
 				}
 			++pointer ;
 
@@ -37,16 +50,16 @@ void Scheduler::roundRobin ()
 			else	
 			{
 			pointer = _queue.begin() ;
-			(*pointer)->start(Time);
-		    Time += x;
-			(*pointer) ->stop(Time);
-			if((*pointer)->finished())
-				{
-					_queue.erase(pointer);
-				}			
-			++pointer ;
+			//(*pointer)->start(Time);
+		    //Time += x;
+			//(*pointer) ->stop(Time);
+			//if((*pointer)-> isFinished())
+				//{
+					//_queue.erase(pointer);
+				//}			
+			//++pointer ;
 			}
-			}
+		}
 	}
 		if (_queue.size()==0){
 		avg_waiting_time= total_waiting_time/z;
